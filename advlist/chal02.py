9a24f6a952d8f298de66aad5b91ef7b8a10d02ac
raymond-sde <raymond-sde@gmail.com>
2021-05-10 22:17:04 +0000

    learning about dictionaries

---
+++ advlist/chal02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+icecream = ["flavors", "salty"]
+icecream.append(99)
+name = input("Enter name")
+print(f"{icecream[-1]} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
