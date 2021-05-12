9ad8d1fba2acc0d7862ff2ff0608b00f7dc1e7cb
raymond-sde <raymond-sde@gmail.com>
2021-05-12 16:55:38 +0000

    studying for logic

--- if_else_project/programmer.py
+++ if_else_project/programmer.py
@@ -2,32 +2,32 @@
 # inspired by programmer meme https://i.redd.it/n3g19svtsg211.jpg
 
 people = {
-    1: {
+    "1": {
         "title": "my mom",
         "answer": "play games all day",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620762870/programmer_meme/mom_ea0pse.png"
     },
-    2: {
+    "2": {
         "title": "my friends",
         "answer": "take apart computers",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620762870/programmer_meme/friends_qbtrmq.png"
     },
-    3: {
+    "3": {
         "title": "society",
         "answer": "live a sedentary lifestyle in my moms basement",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620762870/programmer_meme/society_g8w3tp.png"
     },
-    4: {
+    "4": {
         "title": "artists",
         "answer": "code with aweful design choices",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620762869/programmer_meme/artists_gernsm.png"
     },
-    5: {
+    "5": {
         "title": "I",
         "answer": "enter the matrix",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620765726/programmer_meme/i_lrsbhu.png"
     },
-    6: {
+    "6": {
         "title": "actual",
         "answer": "bang my head on errors all day",
         "url": "https://res.cloudinary.com/drtxi16qr/image/upload/v1620765731/programmer_meme/actual_ecd2qo.png"
@@ -43,17 +43,21 @@ def print_menu():
     print("What I do as a Programmer")
     print(f"{h2}What ___ think(s) I do.")
 
-    for i, person in enumerate(people.keys()):
+    for i, person in enumerate(people.values()):
         title = person["title"]
 
-        if person["title"] != "actual":
-            print(f"{h3}[{i + 1}] {person['title']}")
+        if title != "actual":
+            print(f"{h3}[{i + 1}] {title}")
 
     print(f"{h2}What I actually do")
     print(f"{h3}[{people_len}] find out")
     print(f"{h2}Other Options")
     print(f"{h3}[{people_len + 1}] quit")
     print("-" * 25)
+
+def print_answer(num):
+    print(f"{people[num]['answer']}")
+    print(f"{people[num]['url']}")
 
 def start():
     answer = " "
@@ -66,19 +70,19 @@ def start():
         answer = input("Your answer--> ")
 
         if answer == "1":
-            print("1")
+            print_answer("1")
         elif answer == "2":
-            print("2")
+            print_answer("2")
         elif answer == "3":
-            print("3")
+            print_answer("3")
         elif answer == "4":
-            print("4")
+            print_answer("4")
         elif answer == "5":
-            print("5")
+            print_answer("5")
         elif answer == "6":
-            print("6")
+            print_answer("6")
         elif answer == "7":
-            print("7")
+            break
         else:
             print(f"Invalid input. Please enter a number {MIN_RANGE}-{MAX_RANGE}")
 
