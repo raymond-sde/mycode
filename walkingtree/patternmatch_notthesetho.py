216999efc052a7231ed62764738ec387ed046155
raymond-sde <raymond-sde@gmail.com>
2021-05-13 21:27:02 +0000

    walking tree

---
+++ walkingtree/patternmatch_notthesetho.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#Author: RZFeeser RZFeeser@alta3.com
+
+"""Script to search for a pattern match"""
+
+import os # used to walk the system
+import fnmatch # for regex pattern matching
+import sys
+
+args = sys.argv
+EXCLUDE = ["/usr", "/var"] ## Don't search in these locations
+
+
+def find(pattern, path):
+    """search through filesystem based on given path location"""
+    result = []
+    for root, dirs, files in os.walk(path, topdown=True):
+        if root in EXCLUDE: # if the root matches the exclude list
+            dirs[:] = [] # remove the directory list for this iteration
+            files[:] = [] # remove the file list for this iteration
+        for name in files: # always perform the nested loop, but it maybe empty
+            if fnmatch.fnmatch(name, pattern): # if match
+                result.append(os.path.join(root, name)) # add to our list
+    return result # return the list
+
+def main():
+    """runtime code"""
+    # lookfor = input("What pattern am I looking for (Example: *.txt or *.cfg) ").lower()
+    # lookwhere = input("What is the path in which I should search? ")
+    print("Results: ", find(args[1].lower(), args[2])) # call function
+
+main()
+
