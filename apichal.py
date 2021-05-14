e15877d2d7ed1d095dd5b76d8a0852656e8fce71
Raymond Aranda <raymond.aranda@gmail.com>
2021-05-14 09:23:37 -0700

    api challenge

---
+++ apichal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# imports always go at the top of your code
+import requests
+
+
+def main():
+    pokemon_name = input("Name a pokemon\n>")
+    pokeapi = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}").json()
+
+    img_url = pokeapi["sprites"]["front_default"]
+    img_data = requests.get(img_url).content
+
+    with open(f"{pokemon_name}.png", "wb") as out_file:
+        out_file.write(img_data)
+
+    print("URL: ", img_url)
+    print("Game Indices Count: ", len(pokeapi["game_indices"]))
+
+    for pokedict in pokeapi["moves"]:
+        print(pokedict["move"]["name"])
+
+
+main()
